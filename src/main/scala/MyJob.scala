4dc99a5fb2998176790ebc6594faf73c51d24a13
Aleksandr Plotnikov <aleksandr_plotnikov@epam.com>
2021-07-06 12:31:55 +0300

    made it simple

--- src/main/scala/MyJob.scala
+++ src/main/scala/MyJob.scala
@@ -4,26 +4,24 @@ import org.apache.spark.sql.functions.lit
 import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 
 object MyJob {
-  val PathToInputData = "/airflowfiles/input/"
-  val PathToOutputData = "/airflowfiles/output/"
-
   private val spark = SparkSession.builder
-//    .master("local[*]")
     .appName("SparkAirflowTask")
+//    .master("local[*]")
     .config("spark.sql.shuffle.partitions", 16)
     .getOrCreate()
 
   import spark.implicits._
 
   def main(args: Array[String]): Unit = {
-    spark
-      .read
-      .parquet(PathToInputData)
-      .withColumn("myNewColumn", lit("42"))
-      .write
-      .mode(SaveMode.Overwrite)
-      .format("com.databricks.spark.avro")
-      .save(PathToOutputData)
+    Seq(
+        (1, "Alex"),
+        (2, "Palex"),
+        (3, "Lalex"),
+        (4, "Vova"),
+        (5, "Nova")
+      ).toDF("id", "name")
+      .show()
+
 
     spark.stop()
   }
